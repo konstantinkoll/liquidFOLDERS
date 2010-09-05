b8e8bfa60a30fbbe3c3fca7395bbf6072ddf58cf
Konstantin Koll <koll@liquidfolders.net>
2010-09-05 07:01:49 +0200

    Bugfix
    Signed-off-by: Konstantin Koll <koll@liquidfolders.net>

--- FileDrop/FileDropWnd.cpp
+++ FileDrop/FileDropWnd.cpp
@@ -366,7 +366,7 @@ void CFileDropWnd::OnRButtonUp(UINT /*nFlags*/, CPoint point)
 	if (popup)
 	{
 		HBITMAP bmp1 = SetMenuItemIcon(*popup, 0, ID_APP_CHOOSEDEFAULTSTORE);
-		HBITMAP bmp2 = SetMenuItemIcon(*popup, 6, ID_APP_NEWSTOREMANAGER);
+		HBITMAP bmp2 = SetMenuItemIcon(*popup, 7, ID_APP_NEWSTOREMANAGER);
 		SetMenuItemBitmap(*popup, 8, HBMMENU_POPUP_CLOSE);
 
 		popup->CheckMenuItem(SC_ALWAYSONTOP, AlwaysOnTop ? MF_CHECKED : MF_UNCHECKED);
