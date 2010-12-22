56006ee5d86aa381f05b98103c1967f6a285c6ce
Konstantin Koll <koll@liquidfolders.net>
2010-12-22 07:05:57 +0100

    Bugfix for jumbo icons
    Signed-off-by: Konstantin Koll <koll@liquidfolders.net>

--- StoreManager/CListView.cpp
+++ StoreManager/CListView.cpp
@@ -388,7 +388,17 @@ void CListView::DrawIcon(CDC& dc, CRect& rect, LFItemDescriptor* i, FVItemData* 
 {
 	const INT List = (d->SysIconIndex>=0) ? 1 : 0;
 	rect.OffsetRect((rect.Width()-m_IconSize[List].cx)/2, (rect.Height()-m_IconSize[List].cy)/2);
-	m_Icons[List]->DrawEx(&dc, (d->SysIconIndex>=0) ? d->SysIconIndex : i->IconID-1, rect.TopLeft(), m_IconSize[List], CLR_NONE, 0xFFFFFF, (i->Type & LFTypeGhosted) ? ILD_BLEND50 : ILD_TRANSPARENT);
+
+	if ((m_IconSize[List].cx<128) || (List==0))
+	{
+		m_Icons[List]->DrawEx(&dc, (List==1) ? d->SysIconIndex : i->IconID-1, rect.TopLeft(), m_IconSize[List], CLR_NONE, 0xFFFFFF, (i->Type & LFTypeGhosted) ? ILD_BLEND50 : ILD_TRANSPARENT);
+	}
+	else
+	{
+		HICON hIcon = m_Icons[List]->ExtractIcon(d->SysIconIndex);
+		DrawIconEx(dc, rect.left, rect.top, hIcon, m_IconSize[1].cx, m_IconSize[1].cy, 0, NULL, DI_NORMAL);
+		DestroyIcon(hIcon);
+	}
 }
 
 void CListView::AttributeToString(LFItemDescriptor* i, UINT Attr, WCHAR* tmpStr, size_t cCount)
