7a3c48a708db35e6e39533c95f5e1531361935cc
Konstantin Koll <koll@liquidfolders.net>
2011-02-14 11:52:36 +0100

    - Added DriveTest project - Minor edits
    Signed-off-by: Konstantin Koll <koll@liquidfolders.net>

---
+++ DriveTest/DriveTest.cpp
@@ -0,0 +1,76 @@
+// ConvertIATA.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include "DriveTest.h"
+#include <io.h>
+#include <wchar.h>
+#include <winioctl.h>
+
+
+// Das einzige Anwendungsobjekt
+//
+
+CWinApp theApp;
+
+unsigned int GetDriveBus(char d)
+{
+	unsigned int res = BusTypeMaxReserved;
+
+	char szBuf[MAX_PATH] = "\\\\?\\ :";
+	szBuf[4] = d;
+	HANDLE hDevice = CreateFileA(szBuf, GENERIC_READ,
+		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+
+	if (hDevice!=INVALID_HANDLE_VALUE)
+	{
+		STORAGE_DEVICE_DESCRIPTOR* pDevDesc = (STORAGE_DEVICE_DESCRIPTOR*)new BYTE[sizeof(STORAGE_DEVICE_DESCRIPTOR) + 512 - 1];
+		pDevDesc->Size = sizeof(STORAGE_DEVICE_DESCRIPTOR)+512-1;
+
+		STORAGE_PROPERTY_QUERY Query;
+		Query.PropertyId = StorageDeviceProperty;
+		Query.QueryType = PropertyStandardQuery;
+
+		DWORD dwOutBytes;
+		if (DeviceIoControl(hDevice, IOCTL_STORAGE_QUERY_PROPERTY,
+			&Query, sizeof(STORAGE_PROPERTY_QUERY), pDevDesc, pDevDesc->Size,
+			&dwOutBytes, NULL))
+			res = pDevDesc->BusType;
+
+		delete[] pDevDesc;
+		CloseHandle(hDevice);
+	}
+
+	return res;
+}
+
+void AnalyseLogicalDrives()
+{
+	DWORD DrivesOnSystem = GetLogicalDrives() & ~3;
+	DWORD Index = 1;
+	char szDriveRoot[] = " :\\";
+
+	for (char cDrive='A'; cDrive<='Z'; cDrive++, Index<<=1)
+	{
+		if (!(DrivesOnSystem & Index))
+			continue;
+
+		szDriveRoot[0] = cDrive;
+		std::cout << cDrive << _T(": ") << GetDriveTypeA(szDriveRoot) << _T(", ") << GetDriveBus(cDrive) << _T("\n");
+	}
+}
+
+INT _tmain(INT /*argc*/, TCHAR* /*argv[]*/, TCHAR* /*envp[]*/)
+{
+	// MFC initialisieren und drucken. Bei Fehlschlag Fehlermeldung aufrufen.
+	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+	{
+		// TODO: Den Fehlercode an Ihre Anforderungen anpassen.
+		_tprintf(_T("Schwerwiegender Fehler bei der MFC-Initialisierung\n"));
+		return 1;
+	}
+
+	AnalyseLogicalDrives();
+
+	return 0;
+}
